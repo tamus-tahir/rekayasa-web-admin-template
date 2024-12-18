a955fdbeee666fe809fb0d974d51158dadb71f56
Tamus Tahir <tamusbintahir@gmail.com>
2024-12-18 11:18:35 +0800

    update build tailwind

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,8 +23,10 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <style>
+        {!! Vite::content('resources/css/app.css') !!}
+    </style>
+
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
@@ -42,6 +44,11 @@
             {{ $slot }}
         </div>
     </div>
+
+    <script>
+        {!! Vite::content('resources/js/app.js') !!}
+    </script>
+
 </body>
 
 </html>
